3f4879f4d6d6ae6c4a40813edfa75fe56ce8308a
mkuwan <mkuwan@msn.com>
2022-08-04 23:15:36 +0900

    AppBar作成: 検索Box作成

--- react-front/src/components/TopBar/index.tsx
+++ react-front/src/components/TopBar/index.tsx
@@ -1,18 +1,58 @@
-import {AppBar, Box, IconButton, Toolbar, Typography} from "@mui/material";
+import {alpha, AppBar, Box, IconButton, InputBase, styled, Toolbar, Typography} from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const theme = createTheme();
 
 theme.typography.h3 = {
-    fontSize: '1.2rem',
+    fontSize: '0.9rem',
     '@media (min-width:600px)': {
-        fontSize: '1.5rem',
+        fontSize: '1.2rem',
     },
     [theme.breakpoints.up('md')]: {
-        fontSize: '2rem',
+        fontSize: '1.5rem',
     },
 };
+
+const Search = styled('div')(({theme}) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {backgroundColor: alpha(theme.palette.common.white, 0.25)},
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    }
+}));
+
+const SearchIconWrapper = styled('div')(({theme}) => ({
+    padding: theme.spacing(0 , 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
 
 export const TopBar = () => {
 
@@ -33,6 +73,13 @@ export const TopBar = () => {
                             不思議な図書館
                         </Typography>
                     </ThemeProvider>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase placeholder={"本を検索..."}
+                                         inputProps={{'aria-label': 'search'}}/>
+                    </Search>
                 </Toolbar>
             </AppBar>
         </Box>
